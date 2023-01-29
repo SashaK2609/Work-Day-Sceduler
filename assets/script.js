92b2b8921e52bc1fc26ee4906e5c0d8f5d02a641
SashaKorobkina <s.dekaiper@gmail.com>
2023-01-29 22:26:49 +0000

    displayd current date

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//display the current date
+let currentDay = $("#currentDay");
+
+currentDay = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").text(currentDay);
+
+
+
+
+
+
+// var currentHour = Date.now().getHours();
+// $('.colorcode').each(function(){
+//     var val = parseInt($(this).prop('id'));
+//     if(val > currentHour && val < currentHour+6){
+//         $(this).css('background-color','Blue');
+//     }else if(val < currentHour && val > currentHour-6){
+//         $(this).css('background-color','Red');
+//     }else if(val === currentHour){
+//         $(this).css('background-color','Green');
+//     }else{
+//         $(this).css('background-color','White');
+//     }
+// });
